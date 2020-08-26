1c9d0a064edc06b0ad829a331910cfc6d03848a1
duc <hohuynhduca7bh123@gmail.com>
2020-08-26 22:28:40 +0700

    test

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Windown_form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainForm mainform = new MainForm();
+            mainform.ShowDialog();
+
+            Console.ReadKey();
+        }
+    }
+}
